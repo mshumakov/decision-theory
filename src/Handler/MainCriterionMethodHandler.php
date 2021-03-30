87a09907b2105d10641b081f71e668f1be69207d
Maksim Shumakov <ms.profile.dev@gmail.com>
2021-03-31 00:59:02 +0500

    main-criterion: Add todo and description (ru).

--- src/Handler/MainCriterionMethodHandler.php
+++ src/Handler/MainCriterionMethodHandler.php
@@ -8,14 +8,50 @@ use MSdev\Component\DecisionTheory\ValueObject\DataSetInterface;
 use MSdev\Component\DecisionTheory\ValueObject\DataSetResult;
 use MSdev\Component\DecisionTheory\ValueObject\DataSetResultInterface;
 
+/**
+ * [Описание]
+ * Суть метода состоит в том, что ЛПР выделяет главный критерий,
+ * а на остальные критерии накладывает требования, чтобы они были
+ * не меньше задаваемых им минимальных (пороговых) значений.
+ *
+ * [Использование]
+ * Этот метод целесообразно применять, когда ЛПP может
+ * обоснованно назначить значения или указать узкие пределы для них.
+ *
+ * [Проблемы]
+ * Недостатки этого метода:
+ *  - Оптимизация проводится по существу по одному критерию,
+ *    а остальные могут принимать значения на границе допустимой области;
+ *  - В методе главного критерия ограничения на критерий могут привести к неразрешимости задачи.
+ */
 class MainCriterionMethodHandler extends Handler
 {
     /**
+     * [Математическая модель]
+     * Первым делом требуется определить исходные переменные математической модели:
+     *  x1 - ключевое показатель по первому объекту
+     *  x2 - ключевое показатель по второму объекту
+     *
+     * [Целевые функции]:
+     *  z1(X) = Ax1 + Bx2 -> max
+     *  z2(X) = Cx1 + Dx2 -> max
+     *
+     * [Итоговая целевая функция]:
+     * E.g., z2(X) = Cx1 + Dx2 -> max, при ограничениях
+     * [
+     *   N1x1 + N2x2 >= t1    (variable)
+     *   N3x1 + N3x2 <= const (e.g. 10)
+     *   N5x1 + N6x2 <= const (e.g. 4)
+     *   N7x1 + N8x2 <= const (e.g. 2.4)
+     *   x1, x2      >= 0.
+     * ]
+     *
      * @inheritDoc
      */
     public function handle(DataSetInterface $dataSet): DataSetResultInterface
     {
         // @todo[mshumakov]: Add logic.
+        // @link https://edu.susu.ru/pluginfile.php/5525155/mod_resource/content/3/L_r_02.pdf
 
         return new DataSetResult();
     }
